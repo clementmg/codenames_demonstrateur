a53c7a925197c8cfb36db45ed03bf0dee4b369b9
Baptiste Roberjot <baptiste.roberjot@student-cs.fr>
2021-04-06 11:11:03 +0200

    fix app.run heroku

--- app.py
+++ app.py
@@ -47,7 +47,9 @@ def callAi():
         return jsonify(message)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run()
 
 # -----------------------------------
 
